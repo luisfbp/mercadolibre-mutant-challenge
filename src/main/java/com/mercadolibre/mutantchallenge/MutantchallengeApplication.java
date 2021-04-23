eeda0ae4b153e736856715b5ad716bcbb042af0d
Felipe <felipebejaranop@gmail.com>
2021-04-23 12:41:38 -0500

    Project skeleton

---
+++ src/main/java/com/mercadolibre/mutantchallenge/MutantchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.mutantchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadolibre/mutantchallenge/MutantchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.mutantchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
